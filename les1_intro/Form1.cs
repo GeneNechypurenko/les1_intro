87e4214bc741b997ddfdeab0e1f063008387faf6
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-10-23 18:41:02 +0300

    Add project files.

---
+++ les1_intro/Form1.cs
@@ -0,0 +1,15 @@
+namespace les1_intro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
